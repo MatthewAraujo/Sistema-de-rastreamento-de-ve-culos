920fc8b4c144b0e46118e0c071dff45c55cf3eb4
MatthewAraujo <matthewaraujo20@gmail.com>
2024-12-10 00:01:19 -0300

    mongoDB

--- nestjs-api/src/app.module.ts
+++ nestjs-api/src/app.module.ts
@@ -1,21 +1,23 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MapsModule } from './maps/maps.module';
 import { ConfigModule } from '@nestjs/config';
 import { RoutesModule } from './routes/routes.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { AppController } from './app.controller';
 
 @Module({
   imports: [
-    MapsModule,
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    RoutesModule,
     PrismaModule,
+    MapsModule,
+    RoutesModule,
   ],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule { }
+
+//container de serviços
